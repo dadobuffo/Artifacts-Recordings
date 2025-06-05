a296cd47c188e0e7716b2df05ddcb719b842d4b0
dadobuffo <dariomenik@gmail.com>
2025-06-05 22:54:37 +0200

    new home banner

--- scripts/general.js
+++ scripts/general.js
@@ -7,3 +7,21 @@ menuToggle.addEventListener("change", () => {
     document.body.style.overflow = "";
   }
 });
+
+window.addEventListener("scroll", () => {
+  const arrow = document.querySelector(".arrow-icon");
+  const maxScroll = 400;
+
+  const scrollY = window.scrollY;
+  const opacity = Math.max(0, 1 - scrollY / maxScroll);
+
+  arrow.style.opacity = opacity;
+});
+
+document.querySelector(".arrow-icon").addEventListener("click", () => {
+  const scrollTarget = window.innerHeight - 100;
+  window.scrollTo({
+    top: scrollTarget,
+    behavior: "smooth",
+  });
+});
